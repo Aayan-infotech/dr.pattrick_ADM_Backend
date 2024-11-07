de23bc6e7e9d343d2a28a28bb65b4899de9e039b
UtkarshAayan <utkarsh.gupta@aayaninfotech.com>
2024-11-07 10:55:44 +0530

    editor

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const PORT = process.env.PORT || 3127
 const MONGO_URL = process.env.MONGO_URL
 const FRONTEND = process.env.FRONTEND
+const path = require('path');
 var app = express();
 
 const bodyParser = require('body-parser');
@@ -14,10 +15,14 @@ var corsOptions = {
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
     Credentials: true
 }
-app.use(bodyParser.json({ limit: '50mb' }));
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 app.use(cors(corsOptions))
 app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 
 
@@ -25,10 +30,13 @@ app.use(cookieParser())
 const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
 const userRoutes = require('./routes/userRoutes');
+const newsRoute = require('./routes/newsRoute');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api', userRoutes);
+app.use('/api/news', newsRoute);
+
 
 //database connect
 mongoose.set("strictQuery", false)
---
+++ routes/newsRoute.js
@@ -0,0 +1,12 @@
+// routes/contentRoutes.js
+const express = require('express');
+const router = express.Router();
+const newsController = require('../controllers/newsController');
+
+router.post('/upload-image', newsController.uploadImage);
+router.get('/', newsController.getContents);
+router.post('/', newsController.addContent);
+router.put('/:id', newsController.updateContent);
+router.delete('/:id', newsController.deleteContent);
+
+module.exports = router;
