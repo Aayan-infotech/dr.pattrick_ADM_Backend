173239772b08608009cebc2113a9371ff58e1506
Rohit Anchal <rkanchal02@gmail.com>
2025-05-23 16:53:55 +0530

    delete function fixed

--- routes/studyAnswerRoutes.js
+++ routes/studyAnswerRoutes.js
@@ -5,6 +5,7 @@ const studyAnswerController = require('../controllers/studyAnswerController');
 router.get("/get-all-answers", studyAnswerController.getAllUserAnswersOfAIQusetions);
 router.get('/get-all-answers/:userId', studyAnswerController.getAnswersByUserId);
 router.get("/get-answer/:answerId", studyAnswerController.getUserAnswersOfAIQusetionsByAnswerId);
+// sjhjs
 router.delete("/delete-answer/:answerId", studyAnswerController.deleteUserAnswersOfAIQusetionsByAnswerId);
 
 module.exports = router;
